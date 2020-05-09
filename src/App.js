1a81230a9a1e46b8bbc85a5ae1057e372f523769
baruishi <valduss@outlook.com>
2020-05-09 21:50:14 +0900

    change to hooks

--- src/App.js
+++ src/App.js
@@ -6,86 +6,86 @@ import { concat } from 'rxjs';
 import Axios from 'axios';
 
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentWord: {
-        id: "",
-        kname: "",
-        onyomi_ja: "",
-      }
-    };
-  }
+const App = () => {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     currentWord: {
+  //       id: "",
+  //       kname: "",
+  //       onyomi_ja: "",
+  //     }
+  //   };
+  // }
 
-  async componentDidMount() {
-    Axios({
-      url:'/graphql',
-      method: 'post',
-      data: {
-        query: `
-        {showFirstRadical {
-          id
-          kname
-          onyomi_ja
-        }}
-        `
-      }
-    })
-    .then((result) => {
-      console.log(result.data.data.showFirstRadical);
-      this.setState({currentWord: result.data.data.showFirstRadical[0]});
-      console.log("state", this.state.currentWord);
-    });
-  }
+  // async componentDidMount() {
+  //   Axios({
+  //     url:'/graphql',
+  //     method: 'post',
+  //     data: {
+  //       query: `
+  //       {showFirstRadical {
+  //         id
+  //         kname
+  //         onyomi_ja
+  //       }}
+  //       `
+  //     }
+  //   })
+  //   .then((result) => {
+  //     console.log(result.data.data.showFirstRadical);
+  //     this.setState({currentWord: result.data.data.showFirstRadical[0]});
+  //     console.log("state", this.state.currentWord);
+  //   });
+  // }
 
 
-  study = () => {
-    console.log("study button clicked");
-    console.log(this.state.currentWord);
-  }
+  // study = () => {
+  //   console.log("study button clicked");
+  //   console.log(this.state.currentWord);
+  // }
   
-  lowestRanking = () => {
-    console.log("entredfg");
-    const rankings = [];
-    this.state.allWords.map( (x) =>{
-      if (typeof x === "number") {
-      rankings.push(x.ranking)}
-    });
-    console.log("RANKNGS", rankings);
-    const minimum = Math.min(rankings);
-    let brake = 0;
-    this.state.allWords.map( (x) =>{
-      if (x.ranking === minimum && brake === 0) {
-        brake++;
-        this.setState({ Nextword: x});
-        console.log(this.state.Nextword);
-      }
-    });
-    console.log(JSON.stringify(rankings));
-  }
+  // lowestRanking = () => {
+  //   console.log("entredfg");
+  //   const rankings = [];
+  //   this.state.allWords.map( (x) =>{
+  //     if (typeof x === "number") {
+  //     rankings.push(x.ranking)}
+  //   });
+  //   console.log("RANKNGS", rankings);
+  //   const minimum = Math.min(rankings);
+  //   let brake = 0;
+  //   this.state.allWords.map( (x) =>{
+  //     if (x.ranking === minimum && brake === 0) {
+  //       brake++;
+  //       this.setState({ Nextword: x});
+  //       console.log(this.state.Nextword);
+  //     }
+  //   });
+  //   console.log(JSON.stringify(rankings));
+  // }
 
-  close = () => {
-    console.log("close button clicked");
-    console.log(this.state.currentWord);
-    this.forceUpdate();
-  }
+  // close = () => {
+  //   console.log("close button clicked");
+  //   console.log(this.state.currentWord);
+  //   this.forceUpdate();
+  // }
 
-  handleConfidenceChange = (e) => {
-    const newRanking = this.state.NextwordIndex + e;
-    //console.log(e.target.value);
-    const index = this.state.NextwordIndex;
-    this.state.allWords[index].ranking += e;
-    this.lowestRanking();
+  // handleConfidenceChange = (e) => {
+  //   const newRanking = this.state.NextwordIndex + e;
+  //   //console.log(e.target.value);
+  //   const index = this.state.NextwordIndex;
+  //   this.state.allWords[index].ranking += e;
+  //   this.lowestRanking();
 
 
-  }
+  // }
 
-  render() {
+  
     return (
       <div className="app" >
         <h1>Bar-anki</h1>
-        {this.state.currentWord.id}<br/>
+        {/* {this.state.currentWord.id}<br/>
         {this.state.currentWord.kname} <br/>
         {this.state.currentWord.onyomi_ja} <br/>
  
@@ -93,8 +93,8 @@ class App extends React.Component {
           
           <button onClick={this.study} >study</button>
           <button onClick={this.close} >close</button>
-        </div>
-        
+        </div> */}
+{/*         
        <div className="flex">
           {this.state.wantToStudy ? (
             <ShowWord nextWord={this.state.Nextword}
@@ -103,14 +103,11 @@ class App extends React.Component {
             />
             ) : (
               ""
-
           )}
-          
-          
-        </div>
+        </div> */}
       </div>
     );
-  }
+  
 }
 
 
